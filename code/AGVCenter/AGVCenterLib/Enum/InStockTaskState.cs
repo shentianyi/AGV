8f3ea790596bf964feb364a54d9374af0688e59f
Charlot <7wangsong@gmail.com>
2016-11-25 17:08:54 +0800

    update db

---
+++ code/AGVCenter/AGVCenterLib/Enum/InStockTaskState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AGVCenterLib.Enum
+{
+    public enum InStockTaskState
+    {
+        Init = 0,
+        WaitingStcok = 1,
+        InStocking = 2,
+        InStocked = 3,
+        Canceled = 4
+    }
+}
